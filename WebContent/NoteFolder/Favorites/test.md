0e470ec14b9f53add02e7e297fe0f4f13275a3f7
xs47604 <xs47604@APACCNSHZJW1864.apac.nsroot.net>
2016-01-28 18:03:28 +0800

    Add the Coolapseable and block quote for Markdown script

--- WebContent/NoteFolder/Favorites/test.md
+++ WebContent/NoteFolder/Favorites/test.md
@@ -1,18 +1,75 @@
-{
-#### 小标题
+#@ 小标@题@
 1. 第一点
 2. 第二点
 3. 第三点
 
-}
+#@ 小标题
+- 第一点
+-第二点
+- 第三点
 
-{
+#@ 小标题
+
+# aaa
+
+> sThe overriding design goal for Markdown's
+The overriding design goal for Markdown's
+> The overridi>ng design goal for Markdown's
+The overriding design goal for Markdown's
+
+http://dillinger.io/
+
+
+
+#### 小标题
+
+	The overriding design goal for Markdown's
+	
+	The overriding design goal for Markdown'sThe overriding design goal for Markdown'sThe overriding design goal for Markdown's
+	The overriding design goal for Markdown'sThe overriding design goal for Markdown'sThe overriding design goal for Markdown's
+	The overriding design goal for Markdown'sThe overriding design goal for Markdown'sThe overriding design goal for Markdown's
+	The overriding design goal for Markdown's
+
+# Dillinger
+### Version
+
+#@ aaa
+Dillinger is a cloud-enabled, mobile-ready, offline-storage.AngularJS powered HTML5 Markdown editor.
+
+  1. Type some Markdown on the left
+  2. See HTML in the right
+  3. Magic
+
+ 
+As [marked](https://www.baidu.com)
+
+*Actually* written in **Markdown**!
+
+- [AngularJS](https://www.baidu.com)
+- [Ace Editor](https://www.baidu.com)
+
+***********************************************************
+
+````	
+code a
+code b
+````	
+
+
+I get 10 times more traffic from [Google][1] than from [Yahoo][2] or [MSN][3].  
+
+[1]: http://google.com/ 
+[2]: http://search.yahoo.com/  "http://google.com/ " 
+[3]: http://search.msn.com/    "MSN Search"
+
+#### 小标题
+1. 第一点
+2. 第二点
+3. 第三点
 #### 小标题
 - 第一点
 - 第二点
 - 第三点
-
-}
 
 # Dillinger
 ### Version
@@ -44,69 +101,14 @@ I get 10 times more traffic from [Google][1] than from [Yahoo][2] or [MSN][3].
 [2]: http://search.yahoo.com/  "http://google.com/ " 
 [3]: http://search.msn.com/    "MSN Search"
 
-{
 #### 小标题
 1. 第一点
 2. 第二点
 3. 第三点
-
-}
-
-{
 #### 小标题
 - 第一点
 - 第二点
 - 第三点
-
-}
-
-# Dillinger
-### Version
-
-Dillinger is a cloud-enabled, mobile-ready, offline-storage.AngularJS powered HTML5 Markdown editor.
-
-  1. Type some Markdown on the left
-  2. See HTML in the right
-  3. Magic
-  
-As [marked](https://www.baidu.com)
-
-*Actually* written in **Markdown**!
-
-- [AngularJS](https://www.baidu.com)
-- [Ace Editor](https://www.baidu.com)
-
-***********************************************************
-
-````	
-code a
-code b
-````	
-
-
-I get 10 times more traffic from [Google][1] than from [Yahoo][2] or [MSN][3].  
-
-[1]: http://google.com/ 
-[2]: http://search.yahoo.com/  "http://google.com/ " 
-[3]: http://search.msn.com/    "MSN Search"
-
-
-{
-#### 小标题
-1. 第一点
-2. 第二点
-3. 第三点
-
-}
-
-{
-#### 小标题
-- 第一点
-- 第二点
-- 第三点
-
-}
-
 # Dillinger
 ### Version
 
--- WebContent/WebFolder/public-lib/jude/jude.ui.markdown.js
+++ WebContent/WebFolder/public-lib/jude/jude.ui.markdown.js
@@ -1,19 +1,87 @@
 (function($){
 	$.fn.jdMarkdown = function(filePath) {
 		var $item = this;
-		$item.addClass('jd-mk-panel');
 		filePath = filePath.replace(/ /g, '%20');
+		var headArray = ['h1','h2','h3','h4','h5','h6'];
+		var imgCollapse = 'WebFolder/public-lib/jude/images/collapse.gif';
+		var imgExpand = 'WebFolder/public-lib/jude/images/expand.gif';
+		$item.addClass('jd-mkd-panel');
+		
+		//#@ Collapseable
+		var createCollapseableMD = function() {
+			$item.find('h1').each(function(){
+				var $h1 = $(this);
+				var html = $h1.html();
+				if(html.substring(0,1) === '@') {
+					$h1.addClass('jd-mkd-collapseable');
+					$h1.html('<span class="jd-mkd-img"><img src="' + imgCollapse + '"></img></span><span>' + html.substring(1, html.length) + '</span><span class="jd-mkd-rightimg jd-mkd-down"></span>');
+					var $next = $h1.next();
+					while(true) {
+						if($next.length <= 0) {
+							break;
+						}
+						var tagName = $next[0].tagName;
+						if(headArray.indexOf(tagName.toLowerCase()) > -1) {
+							$next.before('<div class="jd-mkd-line"></div>');
+							break;
+						} else {
+							$next.hide();
+							$next = $next.next();
+						}
+					}
+				}
+			});
+			
+			$item.find('.jd-mkd-collapseable').unbind('click').click(function(){
+				var $h1 = $(this);
+				var img = $h1.find('img').attr('src');
+				if(imgExpand == img) {
+					$h1.find('img').attr('src', imgCollapse);
+					$h1.find('.jd-mkd-rightimg').removeClass('jd-mkd-up');
+					$h1.find('.jd-mkd-rightimg').addClass('jd-mkd-down');
+				} else if(imgCollapse == img) {
+					$h1.find('img').attr('src', imgExpand);
+					$h1.find('.jd-mkd-rightimg').removeClass('jd-mkd-down');
+					$h1.find('.jd-mkd-rightimg').addClass('jd-mkd-up');
+				}
+				var html = $h1.html();
+				var $next = $h1.next();
+				while(true) {
+					if($next.length <= 0) {
+						break;
+					}
+					var tagName = $next[0].tagName;
+					if(headArray.indexOf(tagName.toLowerCase()) > -1 || $next.attr('class') === 'jd-mkd-line') {
+						break;
+					} else {
+						if(imgExpand == img) {
+							$next.slideUp(100);
+						} else if(imgCollapse == img) {
+							$next.slideDown(100);
+						}
+						$next = $next.next();
+					}
+				}
+			});
+		};
+		// >  Block quotes
+		var createBlockQuotesMD = function() {
+			$item.find('p').each(function(){
+				var $p = $(this);
+				var html = $p.html();
+				if(html.substring(0,4) == '&gt;') {
+					html = html.replace(/&gt;/g, '<br>');
+					$p.html('<blockquote><p>' + html + '</p></blockquote>');
+				}
+			});
+		};
 		$item.load(filePath, function() {
 			var html = $item.html();
 			var converter = new Markdown.Converter();
 			var result = converter.makeHtml(html);
-			result = result.replace(/\<p\>{\<\/p\>/g, '<div class="md-title-collapse">').replace(/\<p\>}\<\/p\>/g, '</div>');
 			$item.html(result);
-			$('.md-title-collapse').children(':not(h1,h2,h3,h4,h5,h6)').hide();
-			$('h1,h2,h3,h4,h5,h6').unbind('click').click(function(){
-				var $item = $(this);
-				$item.parent().children(':not(h1,h2,h3,h4,h5,h6)').slideToggle(300);
-			});
+			createCollapseableMD();
+			createBlockQuotesMD();
 		});
 	};
 })(jQuery);
